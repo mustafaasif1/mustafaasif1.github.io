601e1a0684768f1f810f8fbb8d71337755c1d88e
Muhammad Mustafa Asif <mustafa.asif@commercetools.com>
2025-08-17 15:22:41 +0200

    chore: format and remove react-helmet

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import Footer from "../components/common/footer";
 import Logo from "../components/common/logo";
 import NavBar from "../components/common/navBar";
